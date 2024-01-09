575d99c6d039c49dd2fe0e8ae46994ed8a6f4e17
TurnerSF <Turner_shreve@yahoo.com>
2024-01-08 18:16:25 -0800

    finished product

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,10 +1,13 @@
+import './contact.scss'
 import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
-import './index.scss'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import emailjs from '@emailjs/browser'
+import { MapContainer, Marker, TileLayer, Popup } from 'react-leaflet'
 
 const Contact = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
+  const refForm = useRef()
 
   useEffect(() => {
     const timeoutId = setTimeout(() => {
@@ -16,6 +19,24 @@ const Contact = () => {
       clearTimeout(timeoutId)
     }
   }, [])
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm(
+        'Yahoo',
+        'template_b394udp',
+        refForm.current,
+        'SCqzTtKEiFiPgD7eA'
+      )
+      .then(() => {
+        alert('Message successfully sent!')
+        window.location.reload(false)
+      }, () => {
+        alert('Failed to send the message, please try again')
+      })
+  }
 
   return (
     <>
@@ -37,21 +58,61 @@ const Contact = () => {
             Balancing innovation with structure, I am eager to be part of a
             collaborative team and contribute to impactful projects.
           </p>
-          <div className='contact-form'>
-            <form>
-                <ul>
-                    <li className='half'>
-                        <input type="text" name='name' placeholder='Name' required />
-                    </li>
-                    <li className='half'>
-                        <input type="email" name='email' placeholder='Email' required />
-                    </li>
-                    <li>
-                        <input type="text" name='subject' placeholder='Email' required />
-                    </li>
-                </ul>
+          <div className="contact-form">
+            <form ref={refForm} onSubmit={sendEmail}>
+              <ul>
+                <li className="half">
+                  <input type="text" name="name" placeholder="Name" required />
+                </li>
+                <li className="half">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    type="text"
+                    name="subject"
+                    placeholder="Subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    id="message"
+                  ></textarea>
+                </li>
+                <li>
+                <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
             </form>
           </div>
+        </div>
+        <div className='info-map'>
+          Turner Shreve
+          <br />
+          United States,
+          <br />
+          3679 Pumalo St
+          <br />
+          Highland, CA
+          <span>turner_shreve@yahoo.com</span>
+        </div>
+        <div className='map-wrap'>
+          <MapContainer center={[34.138853284370704, -117.20695414971352]} zoom={13}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={[34.138853284370704, -117.20695414971352]}>
+              <Popup>
+                This is where I reside
+              </Popup>
+            </Marker>
+          </MapContainer>
         </div>
       </div>
       <Loader type="ball-scale-ripple" />
